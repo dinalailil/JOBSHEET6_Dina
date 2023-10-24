2df6a4b002481244932142b095e9b1d7372c4145
dinalailil <145964661+dinalailil@users.noreply.github.com>
2023-10-24 21:04:16 +0700

    Add files via upload

---
+++ Pemilihan2Percobaan109.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Pemilihan2Percobaan109 {
+
+    public static void main (String[] args) {
+        Scanner input09 = new Scanner(System.in);
+        int tahun;
+        System.out.println("Masukkan Tahun");
+        tahun = input09.nextInt();
+        if ((tahun % 4) == 0){
+            if ((tahun % 100) !=0)
+                System.out.println("Tahun Kabisat");
+        }else
+            System.out.println("Bukan Tahun Kabisat");
+     }
+    }
